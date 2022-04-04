32c2b200fe97fe8fee0e1d59707534669a414fae
Topi Kanninen <topi.kanninen@sci.fi>
2022-04-04 17:28:03 +0300

    middleware update

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,4 @@
-const logger = require('/logger')
+const logger = require('./logger')
 
 const requestLogger = (request, response, next) => {
     logger.log('Method:', request.method)
@@ -13,7 +13,7 @@ const unknownEndpoint = (request, response) => {
 }
 
 const errorHandler = (error, request, response, next) => {
-    logger.log(error.message)
+    logger.error(error.message)
     
     if (error.name === 'CastError'){
         return response.status(400).send({ error: 'malformatted id'})
