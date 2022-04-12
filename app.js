a86b4a657bbdc575b0823d3ea40f8247a1f69f92
Topi Kanninen <topi.kanninen@sci.fi>
2022-04-12 16:43:51 +0300

    osa 5 4/4 suurin osa E2E testeistä tehty

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ logger.info('connecting to', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB')
-  })
+})
   .catch((error) => {
-    logger.error('error connection to MongoDB:', error.message)
+  logger.error('error connection to MongoDB:', error.message)
   })
 
 app.use(cors())
@@ -28,6 +28,11 @@ app.use(middleware.requestLogger)
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
